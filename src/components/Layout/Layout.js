ea4665fad4b848f7d27fd9ec3f44f824a97b03e1
Manaswini Kundeti <manaswini.kundeti@gmail.com>
2018-01-02 18:57:13 -0500

    Reusing the backdrop and enhancing the side toolbar working features

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,14 +5,26 @@ import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className = {classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class layout extends Component {
+  state = {
+    showSideDrawer : true;
+  }
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer : false});
+  }
+  render () {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+            open={this.state.showSideDrawer}
+            closed={sideDrawerClosedHandler} />
+        <main className = {classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+}
 
-export default layout;
+export default Layout;
