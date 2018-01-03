ff17d47b22c5e4e1f07ba2fbf4ccc330eb3405de
Manaswini Kundeti <manaswini.kundeti@gmail.com>
2018-01-03 10:36:53 -0500

    Adding BurgerIcon for sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-class layout extends Component {
-  state = {
-    showSideDrawer : true;
-  }
-  sideDrawerClosedHandler = () => {
-    this.setState({showSideDrawer : false});
-  }
-  render () {
-    return (
-      <Aux>
-        <Toolbar />
-        <SideDrawer
-            open={this.state.showSideDrawer}
-            closed={sideDrawerClosedHandler} />
-        <main className = {classes.Content}>
-          {this.props.children}
-        </main>
-      </Aux>
-    )
-  }
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
 }
 
 export default Layout;
