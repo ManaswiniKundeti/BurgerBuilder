ec41dee843d0285af111a5a430ef35f8970e543e
Manaswini Kundeti <manaswini.kundeti@gmail.com>
2018-01-05 13:10:49 -0500

    Error Handing - added withErrorHandler.js file to do robust error handling. withErrorHandler.js can be used as a wrapper around any file , along with axios parameter to implement error handling features.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
@@ -99,9 +100,9 @@ class BurgerBuilder extends Component {
     };
     axios.post('/orders.json',order)
           .then(response => {
-            this.setState({loading : false});
+            this.setState({loading : false,purchasing : false});
           })
-          .catch(error => {this.setState({loading : false});}); 
+          .catch(error => {this.setState({loading : false,urchasing : false});});
 
   }
   render(){
@@ -139,4 +140,4 @@ class BurgerBuilder extends Component {
     );
   }
 }
- export default BurgerBuilder;
+ export default withErrorHandler(BurgerBuilder,axios);
