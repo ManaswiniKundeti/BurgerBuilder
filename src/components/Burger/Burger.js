28b369ea6e8d73c6466f7c8c81cf335377ed2631
Manaswini Kundeti <manaswini.kundeti@gmail.com>
2018-01-23 15:14:10 -0500

    added routing to the burger project

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,10 @@
 import React from 'react' ;
+//import { withRouter } from 'react-router-dom';
 import classes from './Burger.css' ;
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
+  console.log(props);
   let transformedIngredients = Object.keys(props.ingredients)   //transform object of key value pairs into an array of burger ingredients
       .map(igKey => {                                              // where value of object is important to decide how many ingredients i need
         return [...Array(props.ingredients[igKey])].map((_,i) => {  // value of key is important for type of ingredient i need
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,6 +27,7 @@ class BurgerBuilder extends Component {
   }
 
   componentDidMount () {
+    console.log(this.props);
     axios.get('https://react-my-burger-17942.firebaseio.com/ingredients.json')
       .then(response => {
         this.setState({ingredients : response.data});
@@ -88,27 +89,17 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     //alert('You Continue');
-    this.setState({loading : true});
-    const order = {
-      ingredients : this.state.ingredients,
-      price : this.state.totalPrice,
-      customer : {
-        name : 'manaswini',
-        address : {
-          street : 'Summit Arbor Dr',
-          zipcode : '27612',
-          country : 'United States'
-        },
-        email : 'manaswini.kundeti@gmail.com'
-      },
-      deliveryMethod : 'Within30Minutes'
-    };
-    axios.post('/orders.json',order)
-          .then(response => {
-            this.setState({loading : false,purchasing : false});
-          })
-          .catch(error => {this.setState({loading : false,urchasing : false});});
 
+    const queryParams = [];
+    for(let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+    }
+    queryParams.push('price=' + this.state.totalPrice);
+    const queryString = queryParams.join('&');
+    this.props.history.push({
+      pathname : '/checkout',
+      search : '?' + queryString
+    });
   }
   render(){
     const disabledInfo = {
